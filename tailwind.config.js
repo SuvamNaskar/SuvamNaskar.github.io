e762c938ef2bfba179fb1baef99cd19ab3997f7f
Suvam Naskar <suvamnaskar780@gmail.com>
2025-06-12 20:29:47 +0530

    MODIFY: [Project] New Node and Express.JS based portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ["./*.html", "./views/**/*.ejs"], // Add other paths if needed
+	theme: {
+		extend: {
+		fontFamily: {
+			cutive: ['Cutive Mono', 'monospace']
+		},
+		},
+	},
+	plugins: [],
+}
