f70d3ff561c5f0e96df23527285717f9a26988fe
Suvam Naskar <suvamnaskar780@gmail.com>
2025-06-13 13:01:55 +0530

    change v3

--- lambda.js
+++ lambda.js
@@ -1,4 +1,8 @@
-const serverlessExpress = require('@vendia/serverless-express');
-const app = require('./app');
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./app'); // assuming app.js exports Express app
 
-exports.handler = serverlessExpress({ app });
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+};
