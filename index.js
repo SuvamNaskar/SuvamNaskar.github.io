52603a01cd866e08be66ffe12343a351076b22e3
Suvam Naskar <suvamnaskar780@gmail.com>
2025-06-13 12:02:01 +0530

    PROJECT READY TO DEPLOY

--- index.js
+++ index.js
@@ -6,8 +6,29 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+// Middleware to detect device type
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  const userAgent = req.headers['user-agent'];
+
+  const isMobileOrTablet = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+
+  if (isMobileOrTablet) {
+    res.redirect('/m');
+  } else {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  }
+});
+
+app.get('/m', (req, res) => {
+  const userAgent = req.headers['user-agent'];
+
+  const isMobileOrTablet = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+
+  if (isMobileOrTablet) {
+    res.sendFile(path.join(__dirname, 'mobile-index.html'));
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.listen(port, () => {
